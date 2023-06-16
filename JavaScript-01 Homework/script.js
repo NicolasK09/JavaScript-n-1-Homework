c5e2301c34962c6a0dad1aff799ef454fdad4e01
NicolasK09 <128836456+NicolasK09@users.noreply.github.com>
2023-06-16 23:42:13 +0200

    Add files via upload

---
+++ JavaScript-01 Homework/script.js
@@ -0,0 +1,31 @@
+// Homework 1 
+let number1 = 100 
+if (number1 < 50) 
+
+{ console.log(number1 < 50); 
+
+} else if (number1 > 20) 
+
+{ console.log(number1 > 20); 
+
+} else { console.log("error"); 
+}
+ // Homework 2
+  let y = 'mariam'
+   if (y='mariam') 
+   { console.log('true');
+
+ } else { console.log( 'false');
+
+ } 
+ // Homework 3 
+ let userName = "mariam";
+
+  switch (userName) 
+
+  { case "mariam": console.log(true); 
+  break;
+  
+  default:
+ console.log("error"); 
+}
